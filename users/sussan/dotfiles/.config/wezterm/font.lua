c902ab3f5d92081cb8009875ec9e70d91ea0ecaf
SuSSan@143 <sussan.osc@gmail.com>
2024-08-25 23:38:38 +0530

    Modified wezterm lua file to use different fonts for different font weight

--- users/sussan/dotfiles/.config/wezterm/font.lua
+++ users/sussan/dotfiles/.config/wezterm/font.lua
@@ -2,11 +2,46 @@ local wezterm = require("wezterm")
 
 local module = {}
 function module.apply_to_config(config)
-	-- config.font = wezterm.font("LigaCartographCF Nerd Font")
-	config.font = wezterm.font("LigaInputMono Nerd Font")
-	config.font_size = 12.0
-	config.cell_width = 1.0
-	config.line_height = 1.0
+  config.font_size = 12.0
+  config.cell_width = 1.0
+  config.line_height = 1.0
+
+  config.font = wezterm.font('LigaInputMono Nerd Font', { weight = 'Light' })
+  config.font_rules = {
+    {
+      intensity = 'Bold',
+      italic = true,
+      font = wezterm.font(
+        'LigaCartographCF Nerd Font',
+        {
+          weight = 'Light',
+          style = 'Italic',
+        }
+      ),
+    },
+    {
+      italic = false,
+      intensity = 'Bold',
+      font = wezterm.font(
+        'LigaOperatorMono Nerd Font',
+        {
+          weight = 'Light',
+          style = 'Italic',
+        }
+      ),
+    },
+    {
+      italic = true,
+      intensity = 'Normal',
+      font = wezterm.font(
+        'LigaOperatorMono Nerd Font',
+        {
+          style = 'Italic',
+          weight = 'Light',
+        }
+      ),
+    }
+  }
 end
 
 -- return our module table
